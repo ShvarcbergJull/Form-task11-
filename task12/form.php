b5b8dac1bc4f2be52dcc5e65accd746e2a8dba15
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-06-28 02:11:41 +0800

    Create form.php

---
+++ task12/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thank you</title>
+</head>
+<body> 
+	<h2><font>Ваша заявка на участие отправлена успешно!</font></h2> 
+	<p><a href="/SHU/sign">Отправить заявку ещё раз!</a></p> 
+	
+</body> 
+</html>
