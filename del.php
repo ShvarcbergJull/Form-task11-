4f607b6b04015c3873914d5d1780eaeee986c94a
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-06-27 18:45:26 +0800

    Create del.php

---
+++ del.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	//error_reporting(0);
+	include "script.php";
+	if(empty($_POST['f'])){ 
+		echo ("вы ничего не выбрали"); 
+	} 
+	else{ 
+		$af=$_POST['f']; 
+		$j=count($af); 
+		echo "<p>Файлы: </p>";
+		for ($i = 0;$i < $j;$i++)
+		{
+			str_replace([":", "/"], ["", ""], $af[$i]);
+			echo $af[$i]."-удаление...\n";
+		} 
+		if (delt_test($af))
+		{
+	 		echo "<p><b>Файлы успешно удалены!</b></p>";
+	 	}
+	 	else
+	 	{
+	 		echo "<p><b>Некоторые файлы не были удалены</b></p>";
+	 	}
+	 }
+?>
+
+<form action="admin.php">
+	<p><input type="submit" value="Вернуться к файлам"></p>
+</form>
+</body>
+</html>
